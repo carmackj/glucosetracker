1da3944a4bb9cb203899560b0d22c6deef8f80e5
Joshua Carmack <carmackj@etsu.edu>
2018-03-28 12:33:23 -0400

    Initial commit

---
+++ app/src/main/java/com/mentorsschool/logindemoapp/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.mentorsschool.logindemoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        logout = (Button)findViewById(R.id.btnLogout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(SecondActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
+// u r bad coder
