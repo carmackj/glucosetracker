4f8e2ef58513771571942785cfe877377a2698e0
Joshua Carmack <carmackj@etsu.edu>
2018-03-29 21:55:11 -0400

    Added layout_logs for showing list of logs. Also redid that part of the home to make it a list instead of individual entries

--- app/src/main/java/com/mentorsschool/logindemoapp/HomeActivity.java
+++ app/src/main/java/com/mentorsschool/logindemoapp/HomeActivity.java
@@ -1,13 +1,65 @@
 package com.mentorsschool.logindemoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase db;
+
+    private Button logout;
+    private Button newLog;
+
+    private TextView nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        DatabaseReference dbref = db.getReference("logs");
+
+        nameText = findViewById(R.id.textName);
+        nameText.setText(firebaseAuth.getCurrentUser().getEmail());
+
+
+        //Fill List
+
+
+
+        //End Fill List
+
+
+        newLog = findViewById(R.id.btnNewLog);
+        newLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, LogGlucoseActivity.class));
+            }
+        });
+
+        logout = findViewById(R.id.btnLogout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
